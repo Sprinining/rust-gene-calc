9b479430ff83298eda0de91e7495810cebd08f74
sprinining <sprinining@gmail.com>
2025-07-18 00:25:42 +0800

    chore: 实现几个简单函数的定义

--- genecalculator.cpp
+++ genecalculator.cpp
@@ -1,3 +1,33 @@
 #include "genecalculator.h"
 
-GeneCalculator::GeneCalculator() {}
+GeneCalculator::GeneCalculator() {
+    // 预先分配容量，避免多次动态扩容
+    seeds_.reserve(100);
+}
+
+void GeneCalculator::addSeed(Seed seed) {
+    auto s = std::make_shared<Seed>(std::move(seed));
+    // 如果已有会插入失败
+    if (seeds_set_.insert(s).second) {
+        seeds_.emplace_back(s);
+    }
+}
+
+void GeneCalculator::clearSeeds() {
+    seeds_set_.clear();
+    seeds_.clear();
+    breeding_seeds_.fill(nullptr);
+    offspring_seed_ = Seed{};
+}
+
+void GeneCalculator::calculate() {}
+
+const Seed &GeneCalculator::getOffspringSeed() const { return offspring_seed_; }
+
+int GeneCalculator::calcQuality() const {}
+
+void GeneCalculator::calcOffspringSeed() {}
+
+const array<shared_ptr<Seed>, 4> &GeneCalculator::getBreedingSeeds() const {
+    return breeding_seeds_;
+}
