64cd215a07c434f2a2868e68c37546329419c155
sprinining <sprinining@gmail.com>
2025-07-18 00:38:32 +0800

    chore: 实现计算种子优劣的函数

--- genecalculator.cpp
+++ genecalculator.cpp
@@ -24,7 +24,36 @@ void GeneCalculator::calculate() {}
 
 const Seed &GeneCalculator::getOffspringSeed() const { return offspring_seed_; }
 
-int GeneCalculator::calcQuality() const {}
+int GeneCalculator::calcQuality() const {
+    int countG = 0, countY = 0, countH = 0, countW = 0, countX = 0;
+    for (auto g : offspring_seed_.genes_) {
+        switch (g) {
+        case AppConsts::GeneType::G:
+            ++countG;
+            break;
+        case AppConsts::GeneType::Y:
+            ++countY;
+            break;
+        case AppConsts::GeneType::H:
+            ++countH;
+            break;
+        case AppConsts::GeneType::W:
+            ++countW;
+            break;
+        case AppConsts::GeneType::X:
+            ++countX;
+            break;
+        }
+    }
+
+    // 若为最优组合 GGGYYY，直接返回最大分数
+    if (countG == 3 && countY == 3)
+        return 100;
+
+    // 否则按权重计算评分
+    int score = countG * 2 + countY * 3 + countH * 1 - countW * 2 - countX * 1;
+    return score;
+}
 
 void GeneCalculator::calcOffspringSeed() {}
 
